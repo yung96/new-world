299c677b173edc1c7324f9553cbd941bc5a1a15d
S <aa@sss.www>
2026-03-22 01:59:35 +0300

    Fix Docker: mount backend as volume, skip alembic in entrypoint, add --reload

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "[entrypoint] Applying database migrations..."
-alembic upgrade head
-echo "[entrypoint] Migrations applied. Starting application..."
-
+echo "[entrypoint] Starting application..."
 exec "$@"
